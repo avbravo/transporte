0ccdf466c39fa8fe38cfc125795ca29e0f8220d8
avbravo <avbravo@avbravo>
2019-08-29 15:43:26 -0500

    con jmoordbutils0.24 con clase  CSSTimeLine.java que contiene los colores a usares

--- src/main/java/com/avbravo/transporte/controller/TimeLineController.java
+++ src/main/java/com/avbravo/transporte/controller/TimeLineController.java
@@ -10,9 +10,13 @@ import com.avbravo.jmoordb.interfaces.IError;
 import com.avbravo.jmoordb.mongodb.history.services.ErrorInfoServices;
 import com.avbravo.jmoordbutils.DateUtil;
 import com.avbravo.transporteejb.entity.Conductor;
+import com.avbravo.transporteejb.entity.Estatus;
+import com.avbravo.transporteejb.entity.Solicitud;
 import com.avbravo.transporteejb.entity.Vehiculo;
 import com.avbravo.transporteejb.entity.Viaje;
 import com.avbravo.transporteejb.repository.ConductorRepository;
+import com.avbravo.transporteejb.repository.EstatusRepository;
+import com.avbravo.transporteejb.repository.SolicitudRepository;
 import com.avbravo.transporteejb.repository.VehiculoRepository;
 import com.avbravo.transporteejb.repository.ViajeRepository;
 import java.io.Serializable;
@@ -44,6 +48,7 @@ public class TimeLineController implements Serializable, IError {
     // <editor-fold defaultstate="collapsed" desc="fields">  
     private TimelineModel timelineModel;
     private TimelineModel timelineConductorModel;
+    private TimelineModel timelineSolicitudModel;
     private Date fechaDesde;
     private Date fechaHasta;
     // </editor-fold>  
@@ -52,6 +57,10 @@ public class TimeLineController implements Serializable, IError {
     @Inject
     ConductorRepository conductorRepository;
     @Inject
+    EstatusRepository estatusRepository;
+    @Inject
+    SolicitudRepository solicitudRepository;
+    @Inject
     VehiculoRepository vehiculoRepository;
     @Inject
     ViajeRepository viajeRepository;
@@ -59,24 +68,28 @@ public class TimeLineController implements Serializable, IError {
     // <editor-fold defaultstate="collapsed" desc="services">  
     @Inject
     ErrorInfoServices errorServices;
+
     // </editor-fold>  
     // <editor-fold defaultstate="collapsed" desc="init">
-      @PostConstruct
+    @PostConstruct
     public void init() {
         try {
-         //   loadTimeLineVehiculo();
+            //   loadTimeLineVehiculo();
 //    timelineModel.toString(); 
 //          timelineConductorModel.toString();
-         } catch (Exception e) {
+        } catch (Exception e) {
             errorServices.errorMessage(nameOfClass(), nameOfMethod(), e.getLocalizedMessage());
         }
     }// </editor-fold>
-
 
     // <editor-fold defaultstate="collapsed" desc="String loadTimeLineVehiculo()">
     public String loadTimeLineVehiculo() {
         try {
             timelineModel = new TimelineModel();
+            fechaDesde = DateUtil.dateFirtsOfMonth(DateUtil.anioActual(), DateUtil.mesActual());
+
+            fechaHasta = DateUtil.dateLastOfMonth(DateUtil.anioActual(), DateUtil.mesActual());
+// 
             // groups  
             List<Vehiculo> list = vehiculoRepository.findBy(new Document("activo", "si"));
 
@@ -84,17 +97,29 @@ public class TimeLineController implements Serializable, IError {
 
             } else {
                 for (Vehiculo v : list) {
-                   fechaDesde = DateUtil.dateFirtsOfMonth(DateUtil.anioActual(), DateUtil.mesActual());
-             
-                    fechaHasta = DateUtil.dateLastOfMonth(DateUtil.anioActual(), DateUtil.mesActual());
-// 
+
                     Document doc = new Document("activo", "si").append("vehiculo.idvehiculo", v.getIdvehiculo());
                     List<Viaje> viajeList = viajeRepository.findBy(doc, new Document("idviaje", -1));
                     if (viajeList == null || viajeList.isEmpty()) {
 
                     } else {
+//                          div.verde 
+//     div.azul 
+//      div.rojo 
+//     div.celeste
+//    div.naranaja
+//    div.amarillo 
+//    div.morado
+                        String color="naranja";
                         for (Viaje vi : viajeList) {
                             String availability = (vi.getRealizado().equals("si") ? "Realizado" : (vi.getRealizado().equals("no") ? "NoRealizado" : "Cancelado"));
+                            switch(vi.getRealizado()){
+                                case "si":
+                                   availability="Realizado";
+                                   color ="verde";
+                                   break;
+                                
+                            }
                             TimelineEvent event = new TimelineEvent(availability, vi.getFechahorainicioreserva(), vi.getFechahorafinreserva(), true, v.getMarca() + " " + v.getPlaca(), availability.toLowerCase());
                             timelineModel.add(event);
                         }
@@ -105,7 +130,7 @@ public class TimeLineController implements Serializable, IError {
         } catch (Exception e) {
             errorServices.errorMessage(nameOfClass(), nameOfMethod(), e.getLocalizedMessage());
         }
-         return "/pages/timeline/timelinevehiculo.xhtml";
+        return "/pages/timeline/timelinevehiculo.xhtml";
     }
 
     // </editor-fold>  
@@ -113,6 +138,9 @@ public class TimeLineController implements Serializable, IError {
     public String loadTimeLineConductor() {
         try {
             timelineConductorModel = new TimelineModel();
+            fechaDesde = DateUtil.dateFirtsOfMonth(DateUtil.anioActual(), DateUtil.mesActual());
+
+            fechaHasta = DateUtil.dateLastOfMonth(DateUtil.anioActual(), DateUtil.mesActual());
             // groups  
             List<Conductor> list = conductorRepository.findBy(new Document("activo", "si"));
 
@@ -120,10 +148,7 @@ public class TimeLineController implements Serializable, IError {
 
             } else {
                 for (Conductor c : list) {
-              
-                                   fechaDesde = DateUtil.dateFirtsOfMonth(DateUtil.anioActual(), DateUtil.mesActual());
-            
-                    fechaHasta = DateUtil.dateLastOfMonth(DateUtil.anioActual(), DateUtil.mesActual());
+
                     Document doc = new Document("activo", "si").append("conductor.idconductor", c.getIdconductor());
                     List<Viaje> viajeList = viajeRepository.findBy(doc, new Document("idviaje", -1));
                     if (viajeList == null || viajeList.isEmpty()) {
@@ -141,8 +166,44 @@ public class TimeLineController implements Serializable, IError {
         } catch (Exception e) {
             errorServices.errorMessage(nameOfClass(), nameOfMethod(), e.getLocalizedMessage());
         }
-     return "/pages/timeline/timelineconductor.xhtml";
+        return "/pages/timeline/timelineconductor.xhtml";
     }
 
+    // </editor-fold>  
+    // <editor-fold defaultstate="collapsed" desc="String loadTimeLineSolicitud()">
+    public String loadTimeLineSolicitud() {
+         try {
+            timelineSolicitudModel = new TimelineModel();
+            fechaDesde = DateUtil.dateFirtsOfMonth(DateUtil.anioActual(), DateUtil.mesActual());
+
+            fechaHasta = DateUtil.dateLastOfMonth(DateUtil.anioActual(), DateUtil.mesActual());
+// 
+            // groups  
+            List<Estatus> list = estatusRepository.findBy(new Document("activo", "si"));
+
+            if (list == null || list.isEmpty()) {
+
+            } else {
+                for (Estatus e : list) {
+
+                    Document doc = new Document("activo", "si").append("estatus.idestatus", e.getIdestatus());
+                    List<Solicitud> solicitudList = solicitudRepository.findBy(doc, new Document("idsolicitud", -1));
+                    if (solicitudList == null || solicitudList.isEmpty()) {
+
+                    } else {
+                        for (Solicitud s : solicitudList) {
+                            String availability = s.getEstatus().getIdestatus();
+                            TimelineEvent event = new TimelineEvent(availability, s.getFechahorapartida(), s.getFechahoraregreso(), true, s.getEstatus().getIdestatus(), availability.toLowerCase());
+                            timelineSolicitudModel.add(event);
+                        }
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            errorServices.errorMessage(nameOfClass(), nameOfMethod(), e.getLocalizedMessage());
+        }
+        return "/pages/timeline/timelinesolicitud.xhtml";
+    }
     // </editor-fold>  
 }
