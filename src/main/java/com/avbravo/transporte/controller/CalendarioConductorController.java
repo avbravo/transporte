16cb1f73bd06200654210944fda0e9f588fdddee
avbravo <avbravo@avbravo>
2019-11-15 14:54:30 -0500

    calendario de conductores

--- src/main/java/com/avbravo/transporte/controller/CalendarioConductorController.java
+++ src/main/java/com/avbravo/transporte/controller/CalendarioConductorController.java
@@ -257,7 +257,7 @@ public class CalendarioConductorController implements Serializable, IController 
             
             
             prepareScheduleConductor();
-//           prepareScheduleVehiculo()
+
 
         } catch (Exception e) {
             errorServices.errorMessage(nameOfClass(), nameOfMethod(), e.getLocalizedMessage(), e);
